10ab87c29a0b2b514d93191d17973223b89326a8
Harshit-2906 <125068698+Harshit-2906@users.noreply.github.com>
2024-03-15 02:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function fibonacci(num) {
 // your code here
-	if (n == 1) {
+	if (n == 1){
 		return 0;
 	} 
-	else if (n == 2 || n == 3) {
+	else if (n == 2 || n == 3){
 	    return 1;
-   } 
-	else {
+	} 
+	else{
 	    let prev1 = 1, prev2 = 1;
-	    for (let i = 4; i <= n; i++) {
+	    
+		for(let i = 4; i <= n; i++){
 			let next = prev1 + prev2;
 		    prev1 = prev2;
 		    prev2 = next;
